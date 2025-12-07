9753a8ce636cfbb291b6277cf0758013da49b01d
hung121104 <duonghungnguyen111204@gmail.com>
2025-12-07 22:16:54 +0700

    Feat: finishined Prefab loader MVP. Note: currently prefefab loader only load on client(mean two player view is not in sync).

--- Assets/Scripts/MVP/Presenters/PrefabLoaderPresenter.cs
+++ Assets/Scripts/MVP/Presenters/PrefabLoaderPresenter.cs
@@ -1,12 +1,40 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PrefabLoaderPresenter
 {
-    public IPrefabLoaderService prefabLoaderService = new PrefabLoaderService();
+    private readonly IPrefabLoaderService prefabLoaderService;
 
-    internal void ValidatePrefabList(List<GameObject> prefabs, List<GameObject> instances)
+    public PrefabLoaderPresenter() : this(new PrefabLoaderService()) { }
+
+    public PrefabLoaderPresenter(IPrefabLoaderService service)
     {
-        prefabLoaderService.validatePrefabList(prefabs, instances);
+        prefabLoaderService = service ?? throw new ArgumentNullException(nameof(service));
+    }
+
+    public void ValidatePrefabList(List<GameObject> prefabs, List<GameObject> instances)
+    {
+        prefabLoaderService.ValidatePrefabList(prefabs, instances);
+    }
+
+    public void LoadAllPrefabs(List<GameObject> prefabs, List<GameObject> instances, Vector3 zOffset)
+    {
+        prefabLoaderService.LoadAllPrefabs(prefabs, instances, zOffset);
+    }
+
+    public void UnloadAllPrefabs(List<GameObject> instances)
+    {
+        prefabLoaderService.UnloadAllPrefabs(instances);
+    }
+
+    public void LoadPrefabByNumber(int number, List<GameObject> prefabs, List<GameObject> instances, Vector3 zOffset)
+    {
+        prefabLoaderService.LoadPrefabByNumber(number, prefabs, instances, zOffset);
+    }
+
+    public void UnloadPrefabByNumber(int number, List<GameObject> instances)
+    {
+        prefabLoaderService.UnloadPrefabByNumber(number, instances);
     }
 }
--- Assets/Scripts/MVP/Views/prefabLoaderView.cs
+++ Assets/Scripts/MVP/Views/prefabLoaderView.cs
@@ -1,5 +1,5 @@
+using PurrNet;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class prefabLoaderView : MonoBehaviour
@@ -14,7 +14,11 @@ public class prefabLoaderView : MonoBehaviour
     private List<GameObject> instances = new List<GameObject>();
 
     // Plant for future use
+    [SerializeField]
     private Vector3 zOffset = new Vector3(0, 0, 1);
+
+    // Inspector test index you can change at runtime/edit time (1-based)
+    public int testIndex = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,9 +26,31 @@ public class prefabLoaderView : MonoBehaviour
         presenter.ValidatePrefabList(prefabs, instances);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Context menu: load all prefabs
+    [ContextMenu("Load All Prefabs")]
+    public void ContextLoadAllPrefabs()
     {
-        
+        presenter.LoadAllPrefabs(prefabs, instances, zOffset);
+    }
+
+    // Context menu: unload all prefabs
+    [ContextMenu("Unload All Prefabs")]
+    public void ContextUnloadAllPrefabs()
+    {
+        presenter.UnloadAllPrefabs(instances);
+    }
+
+    // Context menu: load prefab by testIndex (set testIndex in Inspector)
+    [ContextMenu("Load Prefab (by testIndex)")]
+    public void ContextLoadPrefabByTestIndex()
+    {
+        presenter.LoadPrefabByNumber(testIndex, prefabs, instances, zOffset);
+    }
+
+    // Context menu: unload prefab by testIndex (set testIndex in Inspector)
+    [ContextMenu("Unload Prefab (by testIndex)")]
+    public void ContextUnloadPrefabByTestIndex()
+    {
+        presenter.UnloadPrefabByNumber(testIndex, instances);
     }
 }
