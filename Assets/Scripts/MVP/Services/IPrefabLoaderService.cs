9b7a27fe2ec53d2abe42794db75ff5f3d4d7cee3
hung121104 <duonghungnguyen111204@gmail.com>
2025-12-07 22:16:54 +0700

    Feat: finishined Prefab loader MVP. Note: currently prefefab loader only load on client(mean two player view is not in sync).

--- Assets/Scripts/MVP/Services/IPrefabLoaderService.cs
+++ Assets/Scripts/MVP/Services/IPrefabLoaderService.cs
@@ -1,8 +1,11 @@
-using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
 public interface IPrefabLoaderService
 {
-    public void validatePrefabList(List<GameObject> prefabs, List<GameObject> instances);
+    void ValidatePrefabList(List<GameObject> prefabs, List<GameObject> instances);
+    void LoadAllPrefabs(List<GameObject> prefabs, List<GameObject> instances, Vector3 zOffset);
+    void UnloadAllPrefabs(List<GameObject> instances);
+    void LoadPrefabByNumber(int number, List<GameObject> prefabs, List<GameObject> instances, Vector3 zOffset);
+    void UnloadPrefabByNumber(int number, List<GameObject> instances);
 }
