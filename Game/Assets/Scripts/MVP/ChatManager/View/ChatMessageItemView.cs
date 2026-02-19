55f9a1c9ce7b6a1bf46764875b7f5e1bf2f10940
boboBoizz <nhanthce171949@fpt.edu.vn>
2026-02-20 02:11:09 +0700

    fix chat UI

--- Game/Assets/Scripts/MVP/ChatManager/View/ChatMessageItemView.cs
+++ Game/Assets/Scripts/MVP/ChatManager/View/ChatMessageItemView.cs
@@ -1,5 +1,6 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class ChatMessageItemView : MonoBehaviour
 {
@@ -12,5 +13,20 @@ public class ChatMessageItemView : MonoBehaviour
         senderText.text = message.SenderName;
         contentText.text = message.Content;
         timeText.text = message.TimeStamp.ToString("HH:mm");
+
+        Canvas.ForceUpdateCanvases();
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(contentText.rectTransform);
+
+        float contentHeight = contentText.preferredHeight;
+
+        float padding = 6f;
+        float minHeight = 22f;
+
+        float finalHeight = Mathf.Max(contentHeight + padding, minHeight);
+
+        RectTransform rt = GetComponent<RectTransform>();
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, finalHeight);
     }
 }
+
