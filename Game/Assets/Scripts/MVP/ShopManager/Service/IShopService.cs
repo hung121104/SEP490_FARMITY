d4041a9c1c13fdd9a948f090c5137dedfb1ba092
boboBoizz <nhanthce171949@fpt.edu.vn>
2026-03-19 21:46:37 +0700

    feat: add buy full stack and logic back to inventory

--- Game/Assets/Scripts/MVP/ShopManager/Service/IShopService.cs
+++ Game/Assets/Scripts/MVP/ShopManager/Service/IShopService.cs
@@ -4,6 +4,6 @@ public interface IShopService
 {
     ShopModel GetShopModel();
     void GenerateDailyItems();
-    bool TryBuyItem(int slotIndex, IInventoryService playerInventory);
+    bool TryBuyItem(int slotIndex, IInventoryService playerInventory, bool buyMaxStack = false);
     bool SellItem(string itemId, int quantity, IInventoryService playerInventory);
 }
