ae3d0bfc35e0e373c900087f8be88bfd32bdbf8a
hung121104 <duonghungnguyen111204@gmail.com>
2026-03-19 13:54:24 +0700

    Feat: Add logout funtion

---
+++ Game/Assets/Scripts/MVP/Authenticate/Views/LogOutView.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class LogOutView : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        var button = GetComponent<UnityEngine.UI.Button>();
+        if (button != null)
+            button.onClick.AddListener(OnLogOutClicked);
+    }
+
+    private void OnLogOutClicked()
+    {
+        Debug.Log("[LogOutView] Log out button clicked.");
+        // If connected, request a clean disconnect and clear session in OnDisconnected callback.
+        if (PhotonNetwork.IsConnected)
+        {
+            Debug.Log("[LogOutView] Disconnecting from Photon...");
+            PhotonNetwork.Disconnect();
+            return;
+        }
+
+        // If not connected, clear authentication immediately.
+        PhotonNetwork.AuthValues = null;
+        ClearToken();
+    }
+    
+    public override void OnDisconnected(Photon.Realtime.DisconnectCause cause)
+    {
+        Debug.Log($"[LogOutView] OnDisconnected: {cause}. Clearing auth/session.");
+        PhotonNetwork.AuthValues = null;
+        ClearToken();
+    }
+    private void OnDestroy()
+    {
+        var button = GetComponent<UnityEngine.UI.Button>();
+        if (button != null)
+            button.onClick.RemoveListener(OnLogOutClicked);
+    }
+
+    private void ClearToken()
+    {
+        SessionManager.Instance.ClearSession();
+    }
+
+}
