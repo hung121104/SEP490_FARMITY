bb660023832b68d878259395b2621b5fe2f2b650
hung121104 <duonghungnguyen111204@gmail.com>
2026-03-28 13:57:48 +0700

    feat(server): add data-integrity guards and transactional world delete

--- server/microservices/admin-service/src/game-data/plant/plant.module.ts
+++ server/microservices/admin-service/src/game-data/plant/plant.module.ts
@@ -3,9 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Plant, PlantSchema } from './plant.schema';
 import { PlantService } from './plant.service';
 import { PlantController } from './plant.controller';
+import { Item, ItemSchema } from '../item/item.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Plant.name, schema: PlantSchema }])],
+  imports: [MongooseModule.forFeature([
+    { name: Plant.name, schema: PlantSchema },
+    { name: Item.name, schema: ItemSchema },
+  ])],
   controllers: [PlantController],
   providers: [PlantService],
   exports: [PlantService],
