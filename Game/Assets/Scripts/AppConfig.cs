f211450076e0cf59aba82846c6bbfeda1241f1d4
baonq010624 <baonq010624@gmail.com>
2026-04-05 15:56:53 +0700

    resolve conflict

--- Game/Assets/Scripts/AppConfig.cs
+++ Game/Assets/Scripts/AppConfig.cs
@@ -4,6 +4,6 @@
 /// </summary>
 public static class AppConfig
 {
-    public const string ApiBaseUrl = "https://sep490farmity-gatewayservice-production.up.railway.app";
-    // public const string ApiBaseUrl = "http://localhost:3000";
+    //public const string ApiBaseUrl = "https://sep490farmity-gatewayservice-production.up.railway.app";
+    public const string ApiBaseUrl = "http://localhost:3000";
 }
